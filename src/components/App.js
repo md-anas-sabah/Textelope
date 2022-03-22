91ab18c1a0ff1736f907cb18d358636e297c9d4b
Md Anas Sabah <mdanassabah@gmail.com>
2022-03-23 02:33:50 +0530

    auth provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-//import { AuthProvider } from "./contexts/AuthContext";
-//import Chats from "./Chats";
+// import { AuthProvider } from "../contexts/AuthContext";
+// import Chats from "./Chats";
 import Login from "./Login";
 
 function App() {
@@ -10,11 +10,11 @@ function App() {
     <div style={{ fontFamily: "Avenir" }}>
       <Router>
         {/* <AuthProvider> */}
-        <Switch>
-          {/* <Route path="/chats" components={Chats} /> */}
-          <Route path="/" component={Login} />
-        </Switch>
-        {/* </ AuthProvider> */}
+          <Switch>
+            {/* <Route path="/chats" components={Chats} /> */}
+            <Route path="/" component={Login} />
+          </Switch>
+        {/* </AuthProvider> */}
       </Router>
     </div>
   );
